871e0364e32e106cc4060e8d56a3a75a81fe60a1
ajay2603 <j.a.r.raju2003@gmail.com>
2026-07-31 13:01:54 +0530

    Initial commit 1

---
+++ validate.py
@@ -0,0 +1,14 @@
+import os
+
+required_files=[
+    "index.html",
+    "style.css"
+]
+
+for file in required_files:
+
+    if not os.path.exists(file):
+
+        raise Exception(f"{file} Missing")
+
+print("Validation Successful")
